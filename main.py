8b178ba7a01d9620a0eac049997d8bd0cbcee74a
twined <64403281+twinedd@users.noreply.github.com>
2024-09-29 02:15:52 +0300

    Update main.py
    improved + fixes

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-import random,webbrowser
+import random
 import requests
 import threading
 from dhooks import Webhook
 
+hook = Webhook('discord webhook url here')
 
 def feedback():
     return random.randint(10_000_000, 999_999_999)
 
-
 hosts = ('05', '04', '03', '02', '01')
-hook = Webhook('webhook url')
-
 
 def carnage_finder():
     while True:
         id_1 = str(feedback())
         for i in hosts:
-            photo = requests.get(f'https://feedback{i}.wbbasket.ru/vol{id_1[:4] if len(id_1) > 8 else id_1[:3]}/part{id_1[:6] if len(id_1) > 8 else id_1[:5]}/{id_1}/photos/fs.webp')
-            if photo.status_code != 404:
+            try:
+                photo = requests.get(f'https://feedback{i}.wbbasket.ru/vol{id_1[:4] if len(id_1) > 8 else id_1[:3]}/part{id_1[:6] if len(id_1) > 8 else id_1[:5]}/{id_1}/photos/fs.webp')
+                if photo.status_code != 404:
+                    hook.send(photo.url)
                 print(photo.url)
+            except:
+                pass
 
 def start(times: int = 1):
     for i in range(times):
         threading.Thread(target=carnage_finder).start()
 
-
 start(times=10)
+
+
